73101d9069f0c3ef92f3a876e3fe987b6444cb8f
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-12-03 19:01:47 +0900

    Add : Criのコントロールパネル

---
+++ Assets/Scripts/CommonSystem/AudioManager/CriAudioPlayer/VolumeControlSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// ボリュームコントロールの設定クラス
+/// </summary>
+[System.Serializable]
+public class VolumeControlSettings
+{
+    public string Label;       // UIに表示するラベル
+   [SerializeField,Range(0,100)] public float InitialValue; // 初期音量（0.0～1.0）
+    public SoundType SoundType; // 対応するサウンドタイプ
+    public string CueSheet;   // キューシート名
+    public string CueName;    // キュー名
+}
