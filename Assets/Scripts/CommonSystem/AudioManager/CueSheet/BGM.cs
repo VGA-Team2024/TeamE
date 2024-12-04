50f899b635a794ce8a2f9790f44c921459f2be59
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-12-04 16:09:41 +0900

    自動生成されたコード 再生されるlistが生成されている

---
+++ Assets/Scripts/CommonSystem/AudioManager/CueSheet/BGM.cs
@@ -0,0 +1,12 @@
+﻿///<summary></summary>
+namespace Nirvana.BGM {
+	public class BGM {
+		///<summary></summary>
+		public const int battlebgm = (0);
+	}
+
+	public enum Cue {
+		///<summary></summary>
+		battlebgm = 0,
+	}
+}
