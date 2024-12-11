d3dd139e16fdfa7e5b0863f5ba878c28ce934fdc
Kitamura Haruki <90311063+629Kitamura@users.noreply.github.com>
2024-12-11 18:22:02 +0900

    [fix]タイトル画面からフェードして遷移するように変更

---
+++ Assets/Scripts/GameSystem/SceneTransitions/TitleFadeSceneTransition.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleFadeSceneTransition : MonoBehaviour
+{
+    [SerializeField] string _inGameSceneName;
+    [SerializeField] GameObject _fadePanel;
+    Image _fadePanelImage;
+    [Header("FadeTime+WaitTime=待ち時間")]
+    [SerializeField] float _fadeTime;
+    [SerializeField] float _waitTime;
+
+    void Start()
+    {
+        _fadePanelImage = _fadePanel.GetComponent<Image>();
+        _fadePanelImage.color = new Color(0f, 0f, 0f, 0f);
+        _fadePanel.SetActive(false);
+    }
+
+    public void CollCoroutine()
+    {
+        StartCoroutine(SceneChange());
+    }
+
+    IEnumerator SceneChange()
+    {
+        if (_fadeTime != 0f)
+        {
+            _fadePanel.SetActive(true);
+            float deltaTime = 0f;
+            while (deltaTime < _fadeTime)
+            {
+                deltaTime += Time.deltaTime;
+                var a = Mathf.Clamp01(deltaTime / _fadeTime);
+                _fadePanelImage.color = new Color(0f, 0f, 0f, a);
+                yield return null;
+            }
+            _fadePanelImage.color = new Color(0f, 0f, 0f, 1f);
+        }
+        yield return new WaitForSeconds(_waitTime);
+        SceneManager.LoadScene(_inGameSceneName);
+    }
+}
