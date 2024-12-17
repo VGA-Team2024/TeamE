e8bf26e135881236e0604a844ab901279074df0e
Hikanyan <hikanyan0131@gmail.com>
2024-12-18 01:59:40 +0900

    オプション画面MVP
    オプションからセーブデータからロードして、値をVolumeに流し込む

--- Assets/Scripts/CommonSystem/Option/OptionView.cs
+++ Assets/Scripts/CommonSystem/Option/OptionView.cs
@@ -1,7 +1,27 @@
-﻿namespace CommonSystem.Option
+﻿using UnityEngine;
+
+namespace CommonSystem.Option
 {
-    public class OptionView
+    public class OptionView : MonoBehaviour
     {
-        
+        [SerializeField] private GameObject _optionPanel;
+
+        /// <summary>オプションパネルを表示します。</summary>
+        public void ShowOptionPanel()
+        {
+            _optionPanel.SetActive(true);
+        }
+
+        /// <summary>オプションパネルを非表示にします。</summary>
+        public void HideOptionPanel()
+        {
+            _optionPanel.SetActive(false);
+        }
+
+        /// <summary>オプションパネルの状態を切り替えます。</summary>
+        public void ToggleOptionPanel()
+        {
+            _optionPanel.SetActive(!_optionPanel.activeSelf);
+        }
     }
 }
