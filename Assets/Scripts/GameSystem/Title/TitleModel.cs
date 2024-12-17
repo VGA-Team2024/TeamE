93d0a2f6f119142d28a39d0533a0b55d3765f730
Hikanyan <hikanyan0131@gmail.com>
2024-12-18 04:31:43 +0900

    タイトルのMVP

---
+++ Assets/Scripts/GameSystem/Title/TitleModel.cs
@@ -0,0 +1,20 @@
+﻿namespace GameSystem.Title
+{
+    public class TitleModel
+    {
+        public void StartButton()
+        {
+        }
+
+        public void OptionButton()
+        {
+        }
+
+        public void ExitButton()
+        {
+            // ゲーム終了前のメッセージを表示する
+            // ゲーム終了
+            UnityEngine.Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/GameSystem/Title/TitlePresenter.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace GameSystem.Title
+{
+    public class TitlePresenter : MonoBehaviour
+    {
+        [SerializeField] private TitleView _titleView;
+        [SerializeField] private OptionPresenter _optionPresenter;
+        private TitleModel _titleModel;
+
+        private void Start()
+        {
+            _titleModel = new TitleModel();
+
+            // ボタンイベントの登録
+            _titleView.SetButtonListeners(
+                _titleModel.StartButton,
+                OpenOptionPanel,
+                _titleModel.ExitButton
+            );
+        }
+        
+        /// <summary>
+        /// オプションパネルを開く処理
+        /// </summary>
+        private void OpenOptionPanel()
+        {
+            if (_optionPresenter != null)
+            {
+                _optionPresenter.OpenOptionPanel();
+                Debug.Log("Open Option Panel");
+            }
+            
+        }
+    }
+}
