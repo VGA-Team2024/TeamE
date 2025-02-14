c15c417f13af7095961d7d03aeb051677e7a3499
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2025-02-14 19:38:57 +0900

    タイムラインの再生方法を単純にした

--- Assets/Scripts/GameSystem/Timeline/TimelineContainer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Playables;
-
-public class TimelineContainer : MonoBehaviour
-{
-    [SerializeField] private List<PlayableDirector> _playableDirectors;
-    public List<PlayableDirector> PlayableDirectors => _playableDirectors;
-}
